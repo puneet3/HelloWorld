9a461663d1c32fe19395326356639cbc6a6528f0
puneet3 <56652931+puneet3@users.noreply.github.com>
2019-11-15 14:11:14 -0500

    headlines

---
+++ headlines.java
@@ -0,0 +1,11 @@
+public class DoWhileLoops {
+    public static void main(String[] args)
+    {
+     int counter=1;
+     do{
+        System.out.println(counter);
+        counter++;
+     } while(counter<=5);
+    }
+}
+
